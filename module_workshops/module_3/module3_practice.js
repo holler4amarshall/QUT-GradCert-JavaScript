c189344f4f1832a49589fe06f0fec3c36cc473b6
holly Marshall <holler4amarshall@hotmail.com>
2023-03-04 18:01:20 +1100

    add module 3 practice folder

---
+++ module_workshops/module_3/module3_practice.js
@@ -0,0 +1,8 @@
+/* Create a script that puts the collection of employees and sales into objects, either by using inbuilt functions or creating the objects from scratch. If using the latter method, you can also use your object constructor from last week to help with this, and make sure to put the employees and sales into an array. /*
+
+/* Create a function to traverse all the employees and sales and show information for each. You can reuse the function that displays the formatted information for each from the last workshop. */
+
+/* Create a function that can return the data of one employee or sales objects, identifying them by the employee's ID. */
+
+/* Create a function to find employees and/or sales with a certain property—for example, all salespeople or sales over 10 dollars. */
+
